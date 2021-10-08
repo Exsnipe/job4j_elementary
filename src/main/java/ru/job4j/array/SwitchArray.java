60351a2cf7fdc928b78bec33dfc69b56ece52fbe
Exsnipe <exsnipe@yandex.ru>
2021-10-08 12:29:58 -0700

    class SwitchArray + test

--- src/main/java/ru/job4j/array/SwitchArray.java
+++ src/main/java/ru/job4j/array/SwitchArray.java
@@ -1,6 +1,15 @@
 package ru.job4j.array;
 
 public class SwitchArray {
+   public static int[] swap(int[] array, int source, int dest) {
+       if (source >= 0 && source < array.length && dest >= 0 && dest < array.length) {
+           int buf = array[source];
+           array[source] = array[dest];
+           array[dest] = buf;
+       }
+       return array;
+   }
+
     public static int[] swapBorder(int[] array) {
         int temp = array[0];
         array[0] = array[array.length - 1];
---
+++ src/test/java/ru/job4j/array/SwitchArrayTest.java
@@ -0,0 +1,39 @@
+package ru.job4j.array;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SwitchArrayTest {
+
+    @Test
+    public void whenSwap0To3() {
+        int[] input = new int[] {1, 2, 3, 4};
+        int source = 0;
+        int dest = input.length - 1;
+        int[] result = SwitchArray.swap(input, source, dest);
+        int[] expected = new int[] {4, 2, 3, 1};
+        Assert.assertArrayEquals(expected, result);
+    }
+
+    @Test
+    public void whenSwap3To6() {
+        int[] input = new int[] {3, 7, 3, 3, 7, 1, 9, 4, 2};
+        int source = 3;
+        int dest = 6;
+        int[] result = SwitchArray.swap(input, source, dest);
+        int[] expected = new int[] {3, 7, 3, 9, 7, 1, 3, 4, 2};
+        Assert.assertArrayEquals(expected, result);
+    }
+
+    @Test
+    public void whenNoSwap() {
+        int[] input = new int[] {2, 3, 0, 5};
+        int source = 2;
+        int dest = 4;
+        int[] result = SwitchArray.swap(input, source, dest);
+        int[] expected = new int[] {2, 3, 0, 5};
+        Assert.assertArrayEquals(expected, result);
+    }
+}
