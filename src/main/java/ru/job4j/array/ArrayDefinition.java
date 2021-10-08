84356de1fc40521516e96d4dbe7217d0c88061db
Exsnipe <exsnipe@yandex.ru>
2021-10-07 17:40:50 -0700

    class ArrayDefinition

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -5,8 +5,17 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+        String[] names = new String[4];
         System.out.println(ages.length);
         System.out.println(surnames.length);
         System.out.println(prices.length);
+        names[0] = "Ivan Ivanov";
+        System.out.println(names[0]);
+        names[1] = "Petr Petrov";
+        System.out.println(names[1]);
+        names[2] = "Fedor Fedorov";
+        System.out.println(names[2]);
+        names[3] = "Anton Antonov";
+        System.out.println(names[3]);
     }
 }
