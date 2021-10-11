3c7b5d143fd016ea3f9750a05cb997c2a25741b2
Exsnipe <exsnipe@yandex.ru>
2021-10-11 07:36:41 -0700

    class Check

--- src/main/java/ru/job4j/array/Check.java
+++ src/main/java/ru/job4j/array/Check.java
@@ -3,7 +3,7 @@ package ru.job4j.array;
 public class Check {
     public static boolean mono(boolean[] data) {
         boolean flag = data[0];
-        for (int index = 0; index < data.length; index++) {
+        for (int index = 1; index < data.length; index++) {
             if (data[index] != flag) {
                 return false;
             }
