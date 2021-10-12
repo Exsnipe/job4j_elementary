1362948be399d0af807d89f0d04b62e19f4c87de
Exsnipe <exsnipe@yandex.ru>
2021-10-12 07:46:38 -0700

    class EndWith

--- src/main/java/ru/job4j/array/EndsWith.java
+++ src/main/java/ru/job4j/array/EndsWith.java
@@ -2,9 +2,12 @@ package ru.job4j.array;
 
 public class EndsWith {
     public static boolean endWith(char[] word, char[]  post) {
-            String str = new String(word);
-            String postStr = new String(post);
-            return str.endsWith(postStr);
+        for (int i = 0; i < post.length; i++) {
+            if (post[post.length - 1 - i] != word[word.length - 1 - i]) {
+                return false;
+            }
+        }
+        return true;
     }
 }
 
