a9a5389b4cc0e9657b049cbce8925711de58a8ef
Exsnipe <exsnipe@yandex.ru>
2021-10-13 13:06:39 -0700

    class SortSelected

--- src/main/java/ru/job4j/array/SortSelected.java
+++ src/main/java/ru/job4j/array/SortSelected.java
@@ -4,10 +4,8 @@ public class SortSelected {
     public static int[] sort(int[] data) {
         for (int index = 0; index < data.length; index++) {
             int min = MiniDiapason.findMin(data, index, data.length - 1);
-            int indexMin = FindLoop.indexOf(data, min, 0, data.length);
-            int buf = data[indexMin];
-            data[indexMin] = data[index];
-            data[index] = buf;
+            int indexMin = FindLoop.indexOf(data, min, index, data.length);
+            data = SwitchArray.swap(data, index, indexMin);
         }
         return data;
     }
