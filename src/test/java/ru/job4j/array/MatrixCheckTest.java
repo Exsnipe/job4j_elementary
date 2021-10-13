babe1a80d025a374b392d328924c427f79686994
Exsnipe <exsnipe@yandex.ru>
2021-10-13 08:52:09 -0700

    class MatrixCheck (method monoVertical)

--- src/test/java/ru/job4j/array/MatrixCheckTest.java
+++ src/test/java/ru/job4j/array/MatrixCheckTest.java
@@ -16,4 +16,16 @@ public class MatrixCheckTest {
         boolean rsl = MatrixCheck.monoHorizontal(board, row);
         Assert.assertTrue(rsl);
     }
+
+    @Test
+    public void whenVerticalHasX() {
+        char[][] board = {
+                {' ', ' ', 'X'},
+                {' ', ' ', 'X'},
+                {' ', ' ', 'X'}
+        };
+        int cell = 2;
+        boolean result = MatrixCheck.monoVertical(board, cell);
+        Assert.assertTrue(result);
+    }
 }
