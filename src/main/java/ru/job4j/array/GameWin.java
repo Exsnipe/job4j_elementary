d60b5fcb01151505bae1188b04fa8af6393affbf
Exsnipe <exsnipe@yandex.ru>
2021-10-14 16:39:50 -0700

    class GameWin

--- src/main/java/ru/job4j/array/GameWin.java
+++ src/main/java/ru/job4j/array/GameWin.java
@@ -4,8 +4,8 @@ public class GameWin {
     public static boolean isWin(char[][] matrix) {
         boolean isWin = false;
         for (int diagonalInd = 0; diagonalInd < 5; diagonalInd++) {
-            if (matrix[diagonalInd][diagonalInd] == 'X') {
-                return MatrixCheck.monoHorizontal(matrix, diagonalInd) || MatrixCheck.monoVertical(matrix, diagonalInd);
+            if (matrix[diagonalInd][diagonalInd] == 'X' && MatrixCheck.monoHorizontal(matrix, diagonalInd) || MatrixCheck.monoVertical(matrix, diagonalInd)) {
+                return true;
             }
         }
         return isWin;
