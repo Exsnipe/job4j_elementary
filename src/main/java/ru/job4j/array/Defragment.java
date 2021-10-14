d8b90ded272710ee75da66035a42cef4c0d7f6eb
Exsnipe <exsnipe@yandex.ru>
2021-10-14 09:32:01 -0700

    class Defragment

---
+++ src/main/java/ru/job4j/array/Defragment.java
@@ -0,0 +1,30 @@
+package ru.job4j.array;
+
+public class Defragment {
+    public static String[] compress(String[] array) {
+        for (int index = 0; index < array.length; index++) {
+            if (array[index] == null) {
+                int point = index;
+                for (int i = index; i < array.length; i++) {
+                    if (array[i] != null) {
+                        SwitchArray.swap(array, point, i);
+                        break;
+                    }
+                }
+            }
+        }
+        return array;
+    }
+
+    public static void main(String[] args) {
+        String[] input = {"I", null, "wanna", null, "be", null, "compressed"};
+        for (int index = 0; index < input.length; index++) {
+            System.out.print(input[index] + " ");
+        }
+        String[] compressed = compress(input);
+        System.out.println();
+        for (int index = 0; index < compressed.length; index++) {
+            System.out.print(compressed[index] + " ");
+        }
+    }
+}
