41c260ecb580b20310041a61b723b4144b71231f
Exsnipe <exsnipe@yandex.ru>
2021-10-15 07:29:21 -0700

    class Defragment

--- src/main/java/ru/job4j/array/Defragment.java
+++ src/main/java/ru/job4j/array/Defragment.java
@@ -4,10 +4,9 @@ public class Defragment {
     public static String[] compress(String[] array) {
         for (int index = 0; index < array.length; index++) {
             if (array[index] == null) {
-                int point = index;
-                for (int i = index; i < array.length; i++) {
+                for (int i = index + 1; i < array.length; i++) {
                     if (array[i] != null) {
-                        SwitchArray.swap(array, point, i);
+                        SwitchArray.swap(array, index, i);
                         break;
                     }
                 }
