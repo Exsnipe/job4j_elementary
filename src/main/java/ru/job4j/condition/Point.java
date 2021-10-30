1e096076a9c21338a4c0d037d7d64755fb68aa42
Exsnipe <exsnipe@yandex.ru>
2021-10-30 07:58:33 -0700

    Refactored class Point

--- src/main/java/ru/job4j/condition/Point.java
+++ src/main/java/ru/job4j/condition/Point.java
@@ -1,17 +1,15 @@
 package ru.job4j.condition;
 
 public class Point {
-    public static double distance(int x1, int y1, int x2, int y2) {
-        double dist = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
-        return dist;
+    private int x;
+    private int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
     }
 
-    public static void main(String[] args) {
-        double dist = Point.distance(1, 0, 0, 0);
-        System.out.println("result (1,0) to (0,0) is " + dist);
-        dist  = Point.distance(2, 4, 7, 2);
-        System.out.println("result (2,4) to (7,2) is " + dist);
-        dist  = Point.distance(3, 1, 5, 4);
-        System.out.println("result (3,1) to (5,4) is " + dist);
+    public double distance(Point anotherPoint) {
+        return Math.sqrt(Math.pow(anotherPoint.x - this.x, 2) + Math.pow(anotherPoint.y - this.y, 2));
     }
 }
--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -9,44 +9,37 @@ public class PointTest {
 
     @Test
     public void whenXone0Yone0Xtwo2Ytwo0() {
-        int x1 = 0;
-        int y1 = 0;
-        int x2 = 2;
-        int y2 = 0;
-        double rsl = Point.distance(x1, y1, x2, y2);
+        Point a = new Point(0, 0);
+        Point b = new Point(2, 0);
+        double rsl = a.distance(b);
         double expected = 2;
         Assert.assertEquals(expected, rsl, 0.01);
     }
 
     @Test
     public void whenXone0Yone2Xtwo1Ytwo2Then1() {
-        int x1 = 0;
-        int y1 = 2;
-        int x2 = 1;
-        int y2 = 2;
-        double rsl = Point.distance(x1, y1, x2, y2);
+        Point a = new Point(0, 2);
+        Point b = new Point(1, 2);
+        double rsl = a.distance(b);
         double expected = 1;
         Assert.assertEquals(expected, rsl, 0.01);
     }
 
     @Test
     public void whenXone2Yone2Xtwo1Ytwo5Then3point162() {
-        int x1 = 2;
-        int y1 = 2;
-        int x2 = 1;
-        int y2 = 5;
-        double rsl = Point.distance(x1, y1, x2, y2);
+        Point a = new Point(2, 2);
+        Point b = new Point(1, 5);
+        double rsl = a.distance(b);
         double expected = 3.162;
         Assert.assertEquals(expected, rsl, 0.01);
     }
 
     @Test
     public void whenXone3Yone2Xtwo6Ytwo4Then3point605() {
-        int x1 = 3;
-        int y1 = 2;
-        int x2 = 6;
-        int y2 = 4;
-        double rsl = Point.distance(x1, y1, x2, y2);
+        Point a = new Point(3, 2);
+        Point b = new Point(6, 4);
+        double rsl = a.distance(b);
+
         double expected = 3.605;
         Assert.assertEquals(expected, rsl, 0.01);
     }
